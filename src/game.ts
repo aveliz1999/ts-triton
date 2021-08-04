40a5362bbf596740ddfea9061c8c0e0981ef9c42
aveliz1999 <github@veliz99.com>
2021-08-04 18:44:14 -0400

    Add keeping track of the universe in TritonGame

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,83 @@ import {TritonClient} from "./client";
 
 type Action = 'Do Nothing' | 'Collect All' | 'Drop All' | 'Collect' | 'Drop' | 'Collect All But' | 'Drop All But' | 'Garrison Star'
 
+export type UniverseData = {
+    fleet_speed: number,
+    paused: boolean,
+    productions: number,
+    tick_fragment: number,
+    now: number,
+    tick_rate: number,
+    production_rate: number,
+    stars_for_victory: number,
+    game_over: number,
+    started: boolean,
+    start_time: number,
+    total_stars: number,
+    production_counter: number,
+    trade_scanned: number,
+    tick: number,
+    trade_cost: number,
+    name: string,
+    player_uid: number,
+    admin: number,
+    turn_based: number,
+    war: number,
+    turn_based_time_out: number,
+
+    fleets: {
+        [key: string]: {
+            l: number,
+            lx: string,
+            ly: string,
+            n: string,
+            o: any,
+            puid: number,
+            st: number,
+            uid: number,
+            w: number,
+            x: string,
+            y: string
+        }
+    },
+    players: {
+        [key: string]: {
+            ai: number,
+            alias: string,
+            avatar: number,
+            conceded: number,
+            huid: number,
+            karma_to_give: number,
+            missed_turns: number,
+            ready: number,
+            regard: number,
+            tech: {
+                [key in 'banking' | 'manufacturing' | 'propulsion' | 'research' | 'scanning' | 'terraforming' | 'weapons']: {
+                    value: number,
+                    level: number
+                }
+            },
+            total_economy: number,
+            total_fleets: number,
+            total_industry: number,
+            total_science: number,
+            total_stars: number,
+            total_strength: number
+        }
+    },
+    stars: {
+        [key: string]: {
+            n: string,
+            puid: number,
+            uid: number,
+            v: string,
+            x: string,
+            y: string
+        }
+    },
+}
+
+
 type ShipOrder = {
     delay: number,
     targetPlanetId: number,
@@ -12,6 +89,7 @@ type ShipOrder = {
 export class TritonGame {
     client: TritonClient;
     id: string;
+    currentUniverse: UniverseData;
 
     constructor(client: TritonClient, gameId: string) {
         this.client = client;
@@ -24,8 +102,9 @@ export class TritonGame {
         })
     }
 
-    getFullUniverse() {
-        return this.order('order', 'full_universe_report');
+    async getFullUniverse() {
+        this.currentUniverse = await this.order('order', 'full_universe_report')
+        return this.currentUniverse;
     }
 
     getIntel() {
