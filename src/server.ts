41e000dc8295c86986a4e42850cd523efebf80eb
aveliz1999 <github@veliz99.com>
2021-08-05 19:13:04 -0400

    Add documentation for server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,25 @@
 import {TritonClient} from "./client";
 
 export class TritonServer {
+    /**
+     * The client to use to interact with the server.
+     */
     client: TritonClient;
 
     constructor(client: TritonClient) {
         this.client = client;
     }
 
+    /**
+     * Get the player information.
+     */
     getPlayer() {
         return this.client.serverRequest('init_player');
     }
 
+    /**
+     * Get the player's open games.
+     */
     getOpenGames() {
         return this.client.serverRequest('open_games');
     }
