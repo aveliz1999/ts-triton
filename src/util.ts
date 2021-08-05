99f38b1d03946bcfa0b581cdba04514dbca8db08
aveliz1999 <github@veliz99.com>
2021-08-05 19:13:59 -0400

    Add documentation for util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,13 @@
 import FormData from "form-data";
 import axios from "axios";
 
+/**
+ * Util function used to send the requests to the server
+ *
+ * @param url The URL to send the request to.
+ * @param body The body object to be encoded.
+ * @param authCookie The auth cookie to use to authenticate with the server.
+ */
 export async function sendPost(url: string, body: {[key: string]: string|number}, authCookie?: string) {
     const form = new FormData();
     Object.entries(body).forEach(([key, value]) => {form.append(key, value)});
